10869be0a6935a8eda8f4bca5035d10c91ecbd81
mehbayat <mehradbayat@gmail.com>
2025-02-19 09:39:06 -0500

    Initial commit for news aggregator repository

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from browser_use import Agent
 
 async def main():
     agent = Agent(
-        task="Go to https://www.aibase.com/ and scrape the 'hot video' section and return in json",
+        task="Visit https://www.reddit.com/r/wallstreetbets/comments/1isyn0e/rklb_106k_662k_started_buying_2_years_ago_took/ and retrieve most useful content",
         llm=ChatAnthropic(model_name='claude-3-5-sonnet-20240620', timeout=25, stop=None)
     )
     result = await agent.run()
